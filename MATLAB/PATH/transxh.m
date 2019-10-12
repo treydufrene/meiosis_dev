a76456ac94e47398be39a1d4468a023d0e7f6a06
Trey Dufrene <dufrenet@my.erau.edu>
2019-10-12 09:21:21 -0700

    added file reqs

---
+++ MATLAB/PATH/transxh.m
@@ -0,0 +1,6 @@
+function [R] = transxh(a)
+    R = [1 0 0 a;
+         0 1 0  0;
+         0 0 1 0;
+         0 0 0 1];
+end
---
+++ MATLAB/PATH/transzh.m
@@ -0,0 +1,6 @@
+function [R] = transzh(d) 
+    R = [1 0 0 0;
+         0 1 0 0;
+         0 0 1 d;
+         0 0 0 1];
+end
