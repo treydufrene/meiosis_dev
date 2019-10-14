8d9af1d994bf2d3ecbc613a298937a5c5fc24078
Trey Dufrene <dufrenet@my.erau.edu>
2019-10-14 16:29:33 -0700

    cleanup

--- MATLAB/MeiosisIK.m
+++ MATLAB/MeiosisIK.m
@@ -2,27 +2,27 @@ function gamma = MeiosisIK(pos,R)
     
     eOff = [0;0;5.25];
     npos = pos - R*eOff;
-    xd = npos(1);
-    yd = npos(2);
-    zd = npos(3);
-    off = 1.54;
-    zoff = 22.08;
+    xc = npos(1);
+    yc = npos(2);
+    zc = npos(3);
+    L1 = 22.08;
+    d = 1;
     L2 = 25;
     L3 = 28;
     
     % Inverse Position
-    t1 = atan2(yd,xd);
-    xc = xd + off*cos(t1);
-    yc = yd + off*sin(t1);
-    zc = zd - zoff;
-    D = (xc^2 + yc^2 + zc^2 - L2^2 - L3^2)/(2*L2*L3);
+    t1 = atan2(yc,xc) - atan2(d,sqrt(xc^2 + yc^2 -d^2));
+    D = (xc^2 + yc^2 - d^2 + (zc - L1)^2 - L2^2 - L3^2)/(2*L2*L3);
     t3 = atan2(-sqrt(1-D^2),D);
-    t2 = atan2(zc,sqrt(xc^2 + yc^2)) - atan2(L3*sin(t3),L2 + L3*cos(t3));
+    t2 = atan2(zc - L1,sqrt(xc^2 + yc^2 - d^2)) - atan2(L3*sin(t3),L2 + L3*cos(t3));
     
     % Inverse Orientation
-    t4 = atan2(-cos(t1)*sin(t2+t3)*R(1,3)-sin(t1)*sin(t2+t3)*R(2,3)+cos(t2+t3)*R(3,3),cos(t1)*cos(t2+t3)*R(1,3)+sin(t1)*cos(t2+t3)*R(2,3)+sin(t2+t3)*R(3,3));
-    t5 = atan2(sqrt(1-(sin(t1)*R(1,3)-cos(t1)*R(2,3))^2),sin(t1)*R(1,3)-cos(t1)*R(2,3))-pi/2;
-    t6 = atan2(sin(t1)*R(1,2)-cos(t1)*R(2,2),-sin(t1)*R(1,1)+cos(t1)*R(2,1))-pi/2;
+    R = [0 1 0;0 0 1;1 0 0]*rotz(t2+t3).'*[1 0 0;0 0 1;0 -1 0]*rotz(t1).'*R;
+    eul = rotm2eul(R,'ZYZ');
+    t4 = eul(1);
+    t5 = eul(2);
+    t6 = eul(3);
+
 
     gamma = [t1,t2,t3,t4,t5,t6].';
 end
---
+++ MATLAB/exiled/MeiosisIK.m
@@ -0,0 +1,28 @@
+function gamma = MeiosisIK(pos,R)
+    
+    eOff = [0;0;5.25];
+    npos = pos - R*eOff;
+    xd = npos(1);
+    yd = npos(2);
+    zd = npos(3);
+    off = 1.54;
+    zoff = 22.08;
+    L2 = 25;
+    L3 = 28;
+    
+    % Inverse Position
+    t1 = atan2(yd,xd);
+    xc = xd + off*cos(t1);
+    yc = yd + off*sin(t1);
+    zc = zd - zoff;
+    D = (xc^2 + yc^2 + zc^2 - L2^2 - L3^2)/(2*L2*L3);
+    t3 = atan2(-sqrt(1-D^2),D);
+    t2 = atan2(zc,sqrt(xc^2 + yc^2)) - atan2(L3*sin(t3),L2 + L3*cos(t3));
+    
+    % Inverse Orientation
+    t4 = atan2(-cos(t1)*sin(t2+t3)*R(1,3)-sin(t1)*sin(t2+t3)*R(2,3)+cos(t2+t3)*R(3,3),cos(t1)*cos(t2+t3)*R(1,3)+sin(t1)*cos(t2+t3)*R(2,3)+sin(t2+t3)*R(3,3));
+    t5 = atan2(sqrt(1-(sin(t1)*R(1,3)-cos(t1)*R(2,3))^2),sin(t1)*R(1,3)-cos(t1)*R(2,3))-pi/2;
+    t6 = atan2(sin(t1)*R(1,2)-cos(t1)*R(2,2),-sin(t1)*R(1,1)+cos(t1)*R(2,1))-pi/2;
+
+    gamma = [t1,t2,t3,t4,t5,t6].';
+end
