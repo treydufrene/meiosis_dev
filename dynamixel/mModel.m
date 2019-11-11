950c7969601b688b70fd54ea363af4b921458639
Trey Dufrene <dufrenet@my.erau.edu>
2019-11-11 10:25:51 -0700

    not sure really

--- dynamixel/mModel.m
+++ dynamixel/mModel.m
@@ -1,13 +1,12 @@
 close all;clear;clc
 
-% desired orientation command
-c = 500;
 numFiles = 1;
+% Initialize Variables
 [damp, wn] = deal(zeros(numFiles,1));
 
 for ii = 1:numFiles
     % load experimental data
-    M = csvread(strcat('AX12A_LOAD_',num2str(ii),'.csv'),5,0);
+    M = csvread(strcat('AX12A_LOAD1_', num2str(ii),'.csv'),5,0);
     % show response
     figure();
     plot(M(:,1),M(:,2))
@@ -29,9 +28,34 @@ for ii = 1:numFiles
     G = tf(wn(ii)^2, [1 2*damp(ii)*wn(ii) wn(ii)^2])
 end
 
+% test load 1
+m = .146;       % mass (kg)
+b = .61277;     % length (m)
+h = .01915;     % height (m)
+% moment of inertia about z'
+Iz = (1/12)*m*(h^2 + b^2);
+
+
+
+
 % [u,t] = gensig('square',2,10,0.1)
 % lsim(G,u,t)
 
 % figure()
 % step(G)
 % stepinfo(G)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
