6d643b37bc3136fc0e946b660023d10655d23e35
Trey Dufrene <dufrenet@my.erau.edu>
2020-01-15 15:46:01 -0700

    added pi gpio pinout

--- pi/gpio.py
+++ pi/gpio.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python
 
+from gpiozero import DigitalInputDevice as DID
+import gpiozero as gpio
 
-print('Test')
+a = DID('GPIO17')
+total = 0
+
+while(1):
+    a.wait_for_active()
+    total = total + 1
+    a.wait_for_inactive()
+    print(total)
+
+'''
+prev = 0
+total = 0
+
+while(1):
+    if a.value != prev:
+        total = total + 1
+        prev = a.value
+    print(total)
+'''
